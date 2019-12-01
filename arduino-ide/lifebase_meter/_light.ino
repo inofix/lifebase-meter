5b38fc133b3238b828061e23e5f510ee177d0af9
Michael Lustenberger <mic@inofix.ch>
2019-12-01 23:20:49 +0100

    Unit Descriptor
    Set the unit descriptor for the new sensor.

--- arduino-ide/lifebase_meter/_light.ino
+++ arduino-ide/lifebase_meter/_light.ino
@@ -11,6 +11,11 @@
     - https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleServer.cpp
     - https://github.com/adafruit/Adafruit_TSL2561
 
+    BLE specs can be found here:
+
+    - https://www.bluetooth.com/specifications/gatt/characteristics/
+    - https://www.bluetooth.com/specifications/assigned-numbers/units/
+
     For copyright and/or -left, warranty, terms of use, and such information,
     please have a look at the LICENSE file in the topmost directory...
 */
@@ -35,9 +40,10 @@ static void init_ble_light(BLEServer* ble_server) {
             BLECharacteristic::PROPERTY_NOTIFY
     );
     light_exposure_characteristic->addDescriptor(new BLE2902());
-//    BLE2904 *desc0 = new BLE2904();
-//    desc0->setUnit(0x27AD);
-//    light_exposure_characteristic->addDescriptor(desc0);
+    // https://www.bluetooth.com/specifications/assigned-numbers/units/
+    BLE2904 *desc0 = new BLE2904();
+    desc0->setUnit(0x2731); // lux
+    light_exposure_characteristic->addDescriptor(desc0);
     light_service->start();
 }
 
